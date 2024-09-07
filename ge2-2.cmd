c3134127e2c00fcfdf3b06a7fd29e78ae3259d17
Student User <student@workstation.lab.example.com>
2024-09-07 12:34:43 -0400

    test

--- ge2-2.cmd
+++ ge2-2.cmd
@@ -1,22 +1,24 @@
 lab start backup-oadp
 oc login -u admin -p redhatocp https://api.ocp4.example.com:6443
 oc whoami --show-console
+Install OADP operator,Volsync Operator
 oc get all -n openshift-operators
 oc project openshift-adp
 vim ~/DO380/labs/backup-oadp/obc-backup.yml 
 oc get obc
 oc apply -f  ~/DO380/labs/backup-oadp/obc-backup.yml 
 oc get obc
-oc extract --to=- cm/backup 
+oc extract --to=/home/student/DO380/labs/backup-oadp cm/backup
 oc get cm/openshift-service-ca.crt -o jsonpath='{.data.service-ca\.crt}' | base64 -w0; echo #EXECUTE IN OTHER TERMINAL
-oc extract --to=- secret/backup
+oc extract --to=/home/student/DO380/labs/backup-oadp secret/backup
 oc get route s3 -n openshift-storage
 vim ~/.s3cfg
 s3cmd la
 vim ~/DO380/labs/backup-oadp/dpa-backup.yml 
 vim ~/DO380/labs/backup-oadp/cloud-credentials 
 oc create secret generic cloud-credentials -n openshift-adp --from-file cloud=~/DO380/labs/backup-oadp/cloud-credentials 
-oc create secret generic restic-enc-key -n openshift-adp --from-literal=RESTIC_PASSWORD=$(openssl rand -base64 24)
+#oc create secret generic restic-enc-key -n openshift-adp --from-literal=RESTIC_PASSWORD=$(openssl rand -base64 24)
+oc apply -f ~/DO380/labs/backup-oadp/dpa-backup.yml
 oc get deploy -n openshift-adp
 oc apply -f ~/DO380/labs/backup-oadp/dpa-backup.yml 
 oc get dpa
@@ -24,7 +26,7 @@ oc get backupStorageLocation
 oc get deploy -n openshift-adp
 oc get backupStorageLocation
 oc get volumesnapshotclass
-for class in  $(oc get volumesnapshotclass -oname); do oc patch $class --type=merge -p '{"deletionPolicy": "Retain"}';done
+#for class in  $(oc get volumesnapshotclass -oname); do oc patch $class --type=merge -p '{"deletionPolicy": "Retain"}';done
 oc label volumesnapshotclass velero.io/csi-volumesnapshot-class="true" --all
 vim ~/DO380/labs/backup-oadp/backup.yml
 oc apply -f  ~/DO380/labs/backup-oadp/backup.yml
